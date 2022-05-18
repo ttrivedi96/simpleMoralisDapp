f3b6c9191a2f622cd3cd6b87996257648316bdad
ttrivedi96 <57979967+ttrivedi96@users.noreply.github.com>
2022-05-18 12:18:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Moralis")
+//Creds:-
+// u:- ttrivedi96@gmail.com 
+//Moralis-MianNet
+//moralis-mainNet server URL:- https://7f6efunb6hyn.usemoralis.com:2053/server
+//moralis-mainNet Application ID:- luZ9IDJo7CBlMp7HnEKs9gIneBngMYBU99Z7fmEk
+
+
+
+ // connect to Moralis server
+ const serverUrl = "https://7f6efunb6hyn.usemoralis.com:2053/server";
+ const appId = "luZ9IDJo7CBlMp7HnEKs9gIneBngMYBU99Z7fmEk";
+ Moralis.start({ serverUrl, appId });
+
+
+
+
